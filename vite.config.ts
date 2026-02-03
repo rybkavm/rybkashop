e70f22ee64b37dcd82f6c339f4b094c2ab1bc879
Viktor <rybkavm96@yandex.ru>
2026-02-04 01:31:19 +0300

    fixing image paths 02

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  base: '/',
   server: {
     host: "::",
     port: 8080,
@@ -11,10 +12,12 @@ export default defineConfig(({ mode }) => ({
       overlay: false,
     },
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [
+    react(), 
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+});
